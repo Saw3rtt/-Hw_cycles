ff2897897714d615f4b295cda473e2b10e0ab58e
Saw3rtt <sawertpra@gmail.com>
2023-05-31 14:27:58 +0300

    Задание 1-7

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         //task1
-        for(int i = 1; i <= 10; i++)
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
-            System.out.println();
+        }
+        System.out.println();
         //task2
-        for(int i = 10; i >= 1; i--)
+        for (int i = 10; i >= 1; i--) {
             System.out.println(i);
-            System.out.println();
+        }
+        System.out.println();
         //task3
-        for(int i = 0; i < 17;i = i + 2)
+        for (int i = 0; i < 17; i = i + 2) {
             System.out.println(i);
-            System.out.println();
+        }
+        System.out.println();
+
         //task4
-        for(int i = 10; i >= -10; i--)
+        for (int i = 10; i >= -10; i--) {
             System.out.println(i);
-            System.out.println();
+        }
+        System.out.println();
+        //task5
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+        System.out.println();
+        //task6
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        System.out.println();
+        //task7
+        for(int i = 1; i <= 512; i = i * 2){
+            System.out.println(i);
+        }
     }
 }
