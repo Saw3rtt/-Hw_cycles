9ebd2a92760f984dadc5d27a1fa21ab62a9d41bb
Saw3rtt <sawertpra@gmail.com>
2023-05-31 14:19:35 +0300

    Задание 1-4

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        for(int i = 1; i <= 10; i++)
+            System.out.println(i);
+            System.out.println();
+        //task2
+        for(int i = 10; i >= 1; i--)
+            System.out.println(i);
+            System.out.println();
+        //task3
+        for(int i = 0; i < 17;i = i + 2)
+            System.out.println(i);
+            System.out.println();
+        //task4
+        for(int i = 10; i >= -10; i--)
+            System.out.println(i);
+            System.out.println();
+    }
+}
